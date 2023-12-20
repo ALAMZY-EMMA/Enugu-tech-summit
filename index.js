a95d7c33dd659e9b02cfd22f0fe1136ec3b0e05c
ALAMZY-EMMA <alamzydave@gmail.com>
2023-12-20 10:30:36 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const main2=document.getElementById('main3');
+const main=document.getElementById('merr');
+const image=document.getElementById('logoimg');
+const clickhere=document.getElementById('click');
+
+
+function show(){
+   main2.classList.add('main2');
+   main.style.display='block';
+   image.style.visibility='hidden';
+   clickhere.style.display='none';
+   
+  
+}
+
+function hide(){
+    main2.classList.remove('main2');
+    main.style.display='none';
+    image.style.visibility='visible';
+    clickhere.style.display='block'; 
+}
